1b68bffe70d39cdd91420a8e054e6bb4ee9d21dc
ninzwapo <ninosaromines.applus@gmail.com>
2026-06-20 15:09:27 +0800

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.game_recorder"
-    compileSdk = 34
+    compileSdk = 33
     ndkVersion = "25.1.8937393"
 
     compileOptions {
@@ -21,7 +21,7 @@ android {
     defaultConfig {
         applicationId = "com.example.game_recorder"
         minSdk = 23
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0.0"
     }
